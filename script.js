a5fd156cd25d4bd1d5a186c4100f46ca109841b6
eddiechens <tw004@hotmail.com>
2014-10-26 22:27:12 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	/*$(.element).click(function(){
+		$(this).getElementsByClassName("element_click");
+		$(this).animate({
+			height:'-=10px'
+		});
+	});*/
+	$('div').mouseenter(function(){
+		$(this).animate({
+			height:'+=10px'
+			});
+	});
+});
