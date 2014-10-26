60c1e96546ccbc9facf7cfea22f65159dd4d69c0
eddiechens <tw004@hotmail.com>
2014-10-27 00:30:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
-	/*$(.element).click(function(){
-		$(this).getElementsByClassName("element_click");
-		$(this).animate({
-			height:'-=10px'
-		});
-	});*/
-	document.body.style.backgroundColor = "#AA0000";
+	document.body.style.backgroundColor = "#CC0000";
+	$('#red').click(function(){
+			document.body.style.backgroundColor = "#AA0000";
+			document.getElementById("element").src = "http://imgur.com/DiTkjLW.jpg";
+			//div.src = "http://imgur.com/DiTkjLW.jpg";
+	});
+	   /*$('#element').mouseenter(function() {
+       $(this).animate({
+           height: '+=10px'
+       });
+   });*/
 
 });
+
