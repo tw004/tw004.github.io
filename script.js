c185fd5cf5f96be9be8e5673ebd2f5067c8df109
eddiechens <tw004@hotmail.com>
2014-10-27 08:13:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	document.body.style.backgroundColor = "#CC0000";
-	$('#red').click(function(){
+	$('#element').click(function(){
 			document.body.style.backgroundColor = "#AA0000";
-			document.getElementById("element").src = "http://imgur.com/DiTkjLW.jpg";
+			$(this).getElementById("element").src = "http://imgur.com/DiTkjLW.jpg";
 			//div.src = "http://imgur.com/DiTkjLW.jpg";
 	});
 	   /*$('#element').mouseenter(function() {
