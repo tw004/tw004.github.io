8479e4a4efb6c4ecb293951c4a421f246ee98afd
eddiechens <tw004@hotmail.com>
2014-10-30 00:04:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 	document.body.style.backgroundColor = "#CC0000";
 	$('#element').click(function(){
 			document.body.style.backgroundColor = "#AA0000";
-			//document.getElementById("element").src = "http://imgur.com/DiTkjLW.jpg";
-			$(this).content('url("http://imgur.com/DiTkjLW.jpg")');
+			document.getElementById("element").src = "http://imgur.com/DiTkjLW.jpg";
+			//$(this).content('url("http://imgur.com/DiTkjLW.jpg")');
 			//div.src = "http://imgur.com/DiTkjLW.jpg";
 	});
 	   /*$('#element').mouseenter(function() {
